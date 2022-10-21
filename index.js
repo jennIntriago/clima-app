2e1ae81f46aa762776f53203edf521b45d68b639
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-10-20 22:28:35 -0500

    Weather App with NodeJS

---
+++ index.js
@@ -0,0 +1,60 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import {
+  inquirerMenu,
+  leerInput,
+  listarLugares,
+  pausa,
+} from "./helpers/inquirer.js";
+import { Busquedas } from "./models/busquedas.js";
+
+const main = async () => {
+  const busquedas = new Busquedas();
+
+  let opt;
+
+  do {
+    opt = await inquirerMenu();
+
+    switch (opt) {
+      case 1:
+        //mostrar mensaje
+        const termino = await leerInput("Ciudad: ");
+
+        //buscar los lugares
+        const lugares = await busquedas.ciudad(termino);
+
+        //seleccionar el lugar
+        const id = await listarLugares(lugares);
+        const lugarSel = lugares.find((l) => l.id === id);
+
+        // clima
+        const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
+
+        //Mostrar resultados
+        console.clear();
+        console.log("\nInformacion de la ciudad...\n".green);
+        console.log("Ciudad:", lugarSel.nombre.green);
+        console.log("Lat:", lugarSel.lat);
+        console.log("Lng:", lugarSel.lng);
+        console.log("Temperatura:", clima.temp);
+        console.log("Mínima:", clima.min);
+        console.log("Máxima:", clima.max);
+        console.log("Como está el clima:", clima.desc.blue);
+
+        break;
+      case 2:
+        console.log("Soy la opcion 2");
+        break;
+      case 0:
+        console.log("Soy la opcion salir");
+        break;
+
+      default:
+        break;
+    }
+    if (opt !== 0) await pausa();
+  } while (opt !== 0);
+};
+
+main();
---
+++ models/busquedas.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+
+class Busquedas {
+  //propiedades
+  historial = ["Tegucigalpa", "Madrid", "San Jose"];
+
+  constructor() {
+    //TODO: leer BD si existe
+  }
+
+  //definimos un getter para traer los params
+  get paramsMapbox() {
+    return {
+      language: "es",
+      access_token: process.env.MAPBOX_KEY,
+    };
+  }
+
+  get paramsWeather() {
+    return {
+      units: "metric",
+      lang: "es",
+      appid: process.env.OPENWHEATER_KEY,
+    };
+  }
+  //busca ciudad
+  async ciudad(lugar = "") {
+    try {
+      //peticion http
+      const intance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+        params: this.paramsMapbox,
+      });
+
+      const resp = await intance.get();
+
+      return resp.data.features.map((lugar) => ({
+        //regresa un objeto de forma implicita
+        id: lugar.id,
+        nombre: lugar.place_name_es,
+        lng: lugar.center[0],
+        lat: lugar.center[1],
+      }));
+    } catch (error) {
+      console.log("error");
+      return [];
+    }
+  }
+  async climaLugar(lat, lon) {
+    try {
+      const instance = axios.create({
+        baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+        params: { ...this.paramsWeather, lat, lon },
+      });
+
+      const resp = await instance.get();
+      const { weather, main } = resp.data;
+
+      return {
+        desc: weather[0].description,
+        min: main.temp_min,
+        max: main.temp_max,
+        temp: main.temp,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export { Busquedas };
