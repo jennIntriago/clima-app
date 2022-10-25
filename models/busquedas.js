081dd7fb7797eabc5ccd43480953aa60a2971acb
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-10-24 21:54:30 -0500

    Leer base de datos

--- models/busquedas.js
+++ models/busquedas.js
@@ -1,11 +1,22 @@
+import fs from "fs";
+
 import axios from "axios";
 
 class Busquedas {
   //propiedades
   historial = [];
+  dbPath = "./db/database.json";
 
   constructor() {
     //TODO: leer BD si existe
+    this.leerDB();
+  }
+  get historialCapitalizado() {
+    return this.historial.map((lugar) => {
+      let palabras = lugar.split(" ");
+      palabras = palabras.map((p) => p[0].toUpperCase() + p.substring(1));
+      return palabras.join(" ");
+    });
   }
 
   //definimos un getter para traer los params
@@ -76,6 +87,24 @@ class Busquedas {
     this.historial.unshift(lugar.toLowerCase());
 
     // Grabar en DB
+    this.guardarDB();
+  }
+
+  guardarDB() {
+    const payload = {
+      historial: this.historial,
+    };
+    fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+  }
+
+  leerDB() {
+    if (!fs.existsSync(this.dbPath)) {
+      return null;
+    }
+    const info = fs.readFileSync(this.dbPath, { encoding: "utf-8" });
+
+    const data = JSON.parse(info);
+    this.historial = data.historial;
   }
 }
 
